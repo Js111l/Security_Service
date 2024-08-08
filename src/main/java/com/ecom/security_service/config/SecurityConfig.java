a7898de5143c83f8de8c44f3d8a830a77b42059e
Jakub <k.swiercz@protonmail.com>
2024-08-08 18:12:47 +0200

    configured HttpSecurity, created entry point

---
+++ src/main/java/com/ecom/security_service/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.ecom.security_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public JWTFilter jwtFilter(){
+        return new JWTFilter();
+    }
+    @Bean
+    public AuthenticationEntryPoint entryPoint(){
+        return new SecurityEntryPoint();
+    }
+
+    @Bean
+    public void httpSecurity(HttpSecurity http) throws Exception {
+        http.csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login","/register").permitAll()
+                        .anyRequest().authenticated()
+                ).addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(x->x.authenticationEntryPoint(entryPoint()))
+
+        ;
+
+    }
+
+
+}
